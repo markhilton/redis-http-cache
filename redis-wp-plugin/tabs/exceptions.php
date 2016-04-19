884fbc2d9301d7365da314e9bd29957538767e71
root <root@nginx1.crunchgeek.com>
2016-04-19 14:28:13 -0400

    fixed query exception input save

--- redis-wp-plugin/tabs/exceptions.php
+++ redis-wp-plugin/tabs/exceptions.php
@@ -11,8 +11,8 @@
 <p><textarea cols="80" rows="10" name="exceptions"><?php echo join("\r\n", rediscache::$config['exclude']); ?></textarea></p>
 <p>
 	Ignore query parameters: &nbsp;
-    <input type="radio" name="query" value="YES" <?php echo rediscache::$config['status'] != 'NO' ? 'checked' : ''; ?>> YES
-    <input type="radio" name="query" value="NO"  <?php echo rediscache::$config['status'] == 'NO' ? 'checked' : ''; ?>> NO
+    <input type="radio" name="query" value="YES" <?php echo rediscache::$config['query'] != 'NO' ? 'checked' : ''; ?>> YES
+    <input type="radio" name="query" value="NO"  <?php echo rediscache::$config['query'] == 'NO' ? 'checked' : ''; ?>> NO
 </p>
 
 <p><input type="submit" name="exclude" class="button button-primary button-large" value="Exclude"></p>
